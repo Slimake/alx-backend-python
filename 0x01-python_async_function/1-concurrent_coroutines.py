9308d31e955c1b3de3e177f701c994827c1c329d
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-15 12:18:36 +0100

    Add function docstring

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Execute multiple coroutines at the same time with async
+    Return the sorted delays"""
     tasks = [wait_random(max_delay) for i in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
