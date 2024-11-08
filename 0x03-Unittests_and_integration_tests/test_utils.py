6cdb441488975635e790decbf2b105772f066d5f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-08 15:28:29 +0100

    Add type annotation to class method parameters

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 from utils import access_nested_map
 import unittest
 from parameterized import parameterized
+from typing import Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -14,7 +15,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ("test_case_2", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("test_case_3", {"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected) -> None:
+    def test_access_nested_map(
+      self, name: Any,
+      nested_map: Any,
+      path: Any,
+      expected: Any) -> None:
         """Test access_nested_map function
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -23,7 +28,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ("test_case_1", {}, ("a",)),
         ("test_case_2", {"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, name, nested_map, path):
+    def test_access_nested_map_exception(
+      self, name: Any,
+      nested_map: Any,
+      path: Any):
         """Test access_nested_map function to raise KeyError"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
